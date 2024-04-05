8cf0a29f07e8f04c04d3dd3ba16e89a067733c52
lukemullins02 <154556971+lukemullins02@users.noreply.github.com>
2024-04-05 19:07:09 -0400

    Add files via upload

---
+++ src/pages/Hope Tracker/HopeTracker.js
@@ -0,0 +1,31 @@
+"use client";
+import React, { useInsertionEffect, useState } from 'react';
+import User from './HopeText';
+import AddUser from './HopeInput';
+import Hdr from './HopeHdr';
+import UsersLists from './HopeList';
+
+function Home() {
+   
+  const DUMMY_USERS = [
+   
+]
+
+  const [users, setUsers] = useState(DUMMY_USERS);
+
+  const addUserHandler = user => {
+    setUsers((prevUsers) => {
+       return [user, ...prevUsers];
+    });
+  };
+
+  return (
+    <div>
+      <Hdr />
+      <AddUser onSaveUserData ={addUserHandler}/>
+      <UsersLists items = {users}/>
+    </div>
+  );
+}
+
+export default Home;
