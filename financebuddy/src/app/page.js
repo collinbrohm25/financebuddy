277d0e9438ed70356c2283d31f62d7f628d58aca
Collin Brohm <152939925+collinbrohm25@users.noreply.github.com>
2024-04-05 16:29:11 -0400

    Delete financebuddy directory

--- financebuddy/src/app/page.js
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from "./Components/Navbar/Navbar.js";
-
-export default function Home() {
-  return (
-   <Navbar />
-  );
-}
