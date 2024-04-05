e6348e44b533e8cab0db1a6f81b7343d5578b501
Collin Brohm <collin.brohm@midaspeakcap.com>
2024-04-05 19:11:16 -0400

    maybe

--- src/pages/HopeTracker.js
+++ src/pages/HopeTracker.js
@@ -1,10 +1,37 @@
 import React, { useState } from 'react';
+import Card from './Card';
+import Button from './Button';
 
-export default function Hope() {
+const Hope = (props) =>  {
+  const[grade,setGrade] = useState('')
 
+  const gradeHandler = (event) => {
+    setGrade(event.target.value);
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    const newGrade = {
+      grade : grade
+    }
+    props.Hope(newGrade)
+    setGrade('')
+  };
 
   return (
-    <div>
-      </div>
-  );
+    <Card className = "input">
+      <form onSubmit = {submitForm}>
+        <label>Grade</label>
+        <input
+        id = "grade"
+        value = {grade}
+        onChange = {gradeHandler}
+        />
+        <Button type = "submit">Add Grade</Button>
+      </form>
+    </Card>
+  )
+
+  
 }
+export default Hope;
